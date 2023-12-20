5304b1b9438130a843938c8db7758bd5f8fcb167
陈飞 <chenfei@pkpm.com.cn>
2023-12-20 20:44:27 +0800

    add: 添加第三次课内容

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ https://meeting.tencent.com/user-center/shared-record-info?id=816fd502-2b1f-44a4
 
 
 
-第一次课录屏 12.15
+第二次课录屏 12.15
 
 https://meeting.tencent.com/user-center/shared-record-info?id=5359ea18-43e2-4e73-8c23-ae320c3ef2a5&is_webview=1&from=6&app_lang=zh-cn&click_source_for_middle_login=1
+
+
+
+第三次课录屏 12.20
+
+https://meeting.tencent.com/user-center/shared-record-info?id=1aefca1c-1b3d-43d3-8c18-4f949e9eae7e&is_webview=1&from=6&app_lang=zh-cn&click_source_for_middle_login=1
--- project/Project1/Project1/main.cpp
+++ project/Project1/Project1/main.cpp
@@ -9,76 +9,42 @@
 
 #include <iostream>
 #include <vector>
+#include <stack>
 using std::string;
 
-using string = std::basic_string<char>;
+//
 
-void fun2();
+class shape
+{
+public:
+	void area() {
+
+	}
+};
+
+
 int main()
 {
-	int size = 100;
-	int a[10];
-	int* b = new int[size];
 
-	//std::vector<int> vec={ 1,2,3 };
-	std::vector<string> str{ 10 };
-	fun2();
-	str.push_back("112");
 
-	std::cout << str.size() << std::endl;
-	for (auto item : str)
+	const char* str = "Hello World";
+	
+	std::vector<shape> shapes_vec(10);
+	//普通for循环
+	for (std::vector<shape>::iterator it = shapes_vec.begin(); it != shapes_vec.end(); ++it)
 	{
-		std::cout << "item " << item << std::endl;
+		it->area();
+		*it;
 	}
+
+	//范围for
+	for (shape obj : shapes_vec)
+	{
+		obj.area();//修改obj不会修改shapes_vec中的值
+
+	}
+
 
 
 }
 
-void fun2()
-{
-	string str1 = "Hello";
-	string str2 = "World";
-
-
-	const char* str3 = "Hello";
-	const char* str4 = "ni hao";
-
-
-
-	auto re = str3 + str1; //自动转换const char* 到string对象
-	//过程相当于  string temp(str3);
-	//auto re =str1+ temp
-	//auto re = str3 + str4;//指针不能直接相加
-
-	//以上规则不仅仅适用于+号，也适用于== !=
-
-
-	if (str3 == str4) //如果直接比较const char* 比较的是指针的地址
-	{
-		std::cout << "yes" << std::endl;
-	}
-
-	string str5 = "Hello" + str1 + "World"; //错误，字面值不能直接相加
-}
-
-void fun1()
-{
-	string str = "Hello World";
-
-
-	auto size = str.size();
-	auto c = str[2];
-	auto cc = str.c_str(); //将string类型的变量转换成const char*变量
-
-	auto position = str.find("byebye");
-	if (position == std::string::npos) //npos表示的是一个位置
-	{
-		std::cout << "can not find it" << std::endl;
-	}
-	else
-	{
-		std::cout << "has this string" << std::endl;
-	}
-
-	std::cout << position << std::endl;
-}
