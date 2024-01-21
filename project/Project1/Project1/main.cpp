997c8c60ad0f8b5f197ad527cfb62dd09e37b599
陈飞 <chenfei@pkpm.com.cn>
2024-01-21 21:09:05 +0800

    更新课程内容

--- project/Project1/Project1/main.cpp
+++ project/Project1/Project1/main.cpp
@@ -1,20 +1,21 @@
+
 #include <iostream>
+#include <vector>
 using namespace std;
-struct Test
-{
-	char ch;
-	char c;
-	int id;
-};
 
-int main()
-{
-	//std::cout << "Alignment of int: " << alignof(int) << std::endl;
-	//std::cout << "Alignment of double: " << alignof(double) << std::endl;
-	// 可以添加更多类型以检查它们的对齐方式
-	constexpr int st = sizeof(int);
-	constexpr int ss = sizeof(short);
-	constexpr int size = sizeof(Test);
 
-	std::cout << size << std::endl;
+
+int func(int j, int k, int i = 1)
+{
+	i++; //2
 }
+
+
+int main(int argc, char** argv)
+{
+	func(3,4,5);
+}
+
+//函数类型
+
+
